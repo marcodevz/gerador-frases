9a6c3e56cd75fa301656b906674025471bcdb9df
Marco Aurelio de Souza Santos <marco.aurex2021@gmail.com>
2025-11-21 13:43:29 -0300

    Add motivational phrases and random generator function

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+const frases = [
+    "Acredite em você, sempre!",
+    "Passo a passo, você chega lá.",
+    "O segredo do sucesso é começar.",
+    "Você é mais forte do que imagina.",
+    "Não pare até se orgulhar.",
+    "Grandes coisas levam tempo.",
+    "A disciplina te leva onde a motivação não consegue.",
+    "Seu futuro começa hoje, não amanhã.",
+    "Faça o seu melhor, mesmo que ninguém esteja vendo!",
+    "Você consegue, continue!"
+];
+
+function gerarFrase() {
+    const aleatorio = Math.floor(Math.random() * frases.length);
+    document.getElementById("frase").innerText = frases[aleatorio];
+}
 
